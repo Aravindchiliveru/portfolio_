2d8aec8fb79a5b11fcaacb5ac99db2436e73d497
Chiliveru-Aravind <102401493+Chiliveru-Aravind@users.noreply.github.com>
2022-05-31 16:51:25 +0530

    change1

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <header>
+        <div className="container header__container">
+            <h5>Hello I'm</h5>
+            <h1>Chiliveru Aravind</h1>
+            <h5 className="text-light">Full Stack Developer</h5>
+            
+        </div>
+    </header>
+  )
+}
+
+export default Header
