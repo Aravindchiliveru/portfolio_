5ee4cffb439d0627e8a3bce83d757634aecd9cde
Chiliveru-Aravind <102401493+Chiliveru-Aravind@users.noreply.github.com>
2022-06-01 15:59:05 +0530

    change4

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import headerimg from '../../assets/headerimg.webp'
+import headerimg from '../../assets/aravindHeaderImg.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
